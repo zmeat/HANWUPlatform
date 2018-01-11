72530f908a590a4621be80110730d14eca3382d7
Yanmeng <ymeng125@126.com>
2018-01-11 16:18:23 +0800

    add log4j

--- src/com/application/Bootstrap.java
+++ src/com/application/Bootstrap.java
@@ -1,11 +1,14 @@
 package com.application;
 
+import org.apache.log4j.Logger;
 
 public class Bootstrap {
-	
+	private static Logger log = Logger.getLogger(Bootstrap.class);
 	public static void main(String[] args) throws ClassNotFoundException {
+		log.debug("the bootsrap is begin");
 		Container container = new Container();
 		
 //		System.out.println(container.getService(container.getDBPath()));
 	}
 }
+
